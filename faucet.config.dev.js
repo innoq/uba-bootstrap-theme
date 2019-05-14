08655abb891aecc4cc634c1266349f0f58c42ac8
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2019-05-14 15:32:09 +0200

    Add some more dirs to live server and add static files support

--- faucet.config.dev.js
+++ faucet.config.dev.js
@@ -3,6 +3,18 @@ module.exports = {
     {
       source: './dist/css/uba-bootstrap-theme.css',
       target: './documentation/docs/4.3/dist/css/bootstrap.css'
+    },
+    {
+      source: './src/fonts',
+      target: './documentation/docs/4.3/dist/fonts'
+    },
+    {
+      source: './bootstrap/dist/js',
+      target: './documentation/docs/4.3/dist/js'
+    },
+    {
+      source: './static',
+      target: './documentation'
     }
   ],
   sass: [
